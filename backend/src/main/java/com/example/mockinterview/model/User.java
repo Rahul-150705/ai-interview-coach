dd5cadbd5d430e3e144f725a119d76243e814064
Rahul P <rahul.cd23@bitsathy.ac.in>
2025-11-29 19:40:30 +0530

    Update User.java

--- backend/src/main/java/com/example/mockinterview/model/User.java
+++ backend/src/main/java/com/example/mockinterview/model/User.java
@@ -1,21 +1,17 @@
 package com.example.mockinterview.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Data;
 
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String fullName;
-
-    @Column(unique = true)
     private String email;
 
     private String password;
